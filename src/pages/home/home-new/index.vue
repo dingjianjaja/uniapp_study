cc1370968bfabbb2fa462115b016761a62c21934
chensx <mia.s.x.chen@qq.com>
2021-11-28 18:21:16 +0800

    init

---
+++ src/pages/home/home-new/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <view>最新</view>
+</template>
+
+
+<script>
+
+export default ({
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/home-recommend/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <view>推荐</view>
+</template>
+
+
+<script>
+
+export default ({
+})
+</script>
+
+<style>
+
+</style>
